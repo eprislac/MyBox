8a16aaf46a48cf28f36f942892a2e53539c0f7ef
Eddie Prislac <edward.prislac@gmail.com>
2012-07-20 11:39:13 -0500

    Added :uploaded_file and altered app/views/assets/_form.htmnl.erb to reflect the addition.

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,4 +1,7 @@
 class Asset < ActiveRecord::Base
-  attr_accessible :user_id
+  attr_accessible :user_id, :uploaded_file
   belongs_to :user
+  has_attached_file :uploaded_file
+  validates_attachment_size :uploaded_file, :less_than => 100.megabytes
+  validates_attachment_presence :uploaded_file
 end
