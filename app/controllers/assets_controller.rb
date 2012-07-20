96b1ad2e7cffb431b813fe01992b2f81bd0c9b49
Eddie Prislac <edward.prislac@gmail.com>
2012-07-20 12:05:32 -0500

    Added file_name alias definition for uploaded_file_file_name

--- app/controllers/assets_controller.rb
+++ app/controllers/assets_controller.rb
@@ -6,7 +6,7 @@ class AssetsController < ApplicationController
   end
 
   def show
-    @assets = current_user.assets.find(params[:id])
+    @asset = current_user.assets.find(params[:id])
   end
 
   def new
@@ -14,7 +14,7 @@ class AssetsController < ApplicationController
   end
 
   def create
-   @assets = current_user.assets.new(params[:asset])
+   @asset = current_user.assets.new(params[:asset])
     if @asset.save
       redirect_to @asset, :notice => "Successfully created asset."
     else
@@ -23,11 +23,11 @@ class AssetsController < ApplicationController
   end
 
   def edit
-    @asset = current_user.assets.find(params[:id])
+    @asset = current_user.assets.find(params[:id]) 
   end
 
   def update
-    @asset = @assets = current_user.assets.find(params[:id])
+   @asset = current_user.assets.find(params[:id])
     if @asset.update_attributes(params[:asset])
       redirect_to @asset, :notice  => "Successfully updated asset."
     else
@@ -36,7 +36,7 @@ class AssetsController < ApplicationController
   end
 
   def destroy
-    @asset = @assets = current_user.assets.find(params[:id])
+    @asset = current_user.assets.find(params[:id]) 
     @asset.destroy
     redirect_to assets_url, :notice => "Successfully destroyed asset."
   end
--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,4 +5,9 @@ class Asset < ActiveRecord::Base
   has_attached_file :uploaded_file
   validates_attachment_size :uploaded_file, :less_than => 100.megabytes
   validates_attachment_presence :uploaded_file
+  
+  def file_name
+    uploaded_file_file_name
+  end
+  
 end
