a700ae4fade52e48730816eea016ec902f65cef4
Eddie Prislac <edward.prislac@gmail.com>
2012-07-20 14:11:31 -0500

    Added has_many :folders reference to app/models/users.rb, belongs_to :user reference to app/models/folders.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+ has_many :folders
  has_many :assets
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120720162855) do
+ActiveRecord::Schema.define(:version => 20120720190233) do
 
   create_table "assets", :force => true do |t|
     t.integer  "user_id"
@@ -24,6 +24,17 @@ ActiveRecord::Schema.define(:version => 20120720162855) do
   end
 
   add_index "assets", ["user_id"], :name => "index_assets_on_user_id"
+
+  create_table "folders", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  add_index "folders", ["parent_id"], :name => "index_folders_on_parent_id"
+  add_index "folders", ["user_id"], :name => "index_folders_on_user_id"
 
   create_table "users", :force => true do |t|
     t.string   "name",                   :default => "", :null => false
