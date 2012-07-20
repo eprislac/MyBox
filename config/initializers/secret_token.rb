f1a55189f633bc96d44d50625f86a966c47efddf
Eddie Prislac <edward.prislac@gmail.com>
2012-07-20 08:52:59 -0500

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mybox::Application.config.secret_token = 'e6bc6b6591bef6e6690bd253690d2e0d89aad125458afefa2c1c9d34482890ccc5ed05ae2be319e810ba940264676aab9da00a8d2c8260a43ca7cff5df31d1f3'
